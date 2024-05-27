0ef7d990c36efe2ee46224076a151cab8568635f
udhayamkumar <164313193+udhayamkumar@users.noreply.github.com>
2024-05-27 12:18:36 +0000

    Copilot second Commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMembers() {
+    let member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 2000
+    };
+
+    return member;
+}
